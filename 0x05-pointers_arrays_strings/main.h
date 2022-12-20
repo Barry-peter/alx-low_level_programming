73860939496b0b209e18be064a873027373d8624
Barry-peter <greatpizzy@gmail.com>
2022-12-19 18:32:07 -0800

    task 2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,5 +12,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar (char c);
+int _atoi(char *s);
+
 
 #endif
